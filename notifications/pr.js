6ba5324c433fe0d94a0a3eadfd8ca5affc15012e
Tamizhvendan Sembiyan <tamizhvendan.sembiyan@chargebee.com>
2022-03-15 19:27:48 +0530

    make PR slack notification only if the target branch is staging and develop

--- notifications/pr.js
+++ notifications/pr.js
@@ -1,14 +1,27 @@
 const slack = require("./slack")
 
+function isNotAgainstDevelopOrStaging(pr) {
+  return !(pr.to.startsWith("staging") || pr.to.startsWith("develop"))
+}
+
 async function prOpened(pr) {
+  if (isNotAgainstDevelopOrStaging(pr)) {
+    return
+  }
   await slack.notifyNewPR(pr)
 }
 
 async function prClosed(pr, closedBy) {
+  if (isNotAgainstDevelopOrStaging(pr)) {
+    return
+  }
   await slack.notifyClosedPR(pr, closedBy)
 }
 
 async function prMerged(pr, mergedBy) {
+  if (isNotAgainstDevelopOrStaging(pr)) {
+    return
+  }
   await slack.notifyMergedPR(pr, mergedBy)
 }
 
