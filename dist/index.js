62f3760758beffba110cdb74b71c8e22a2f029a5
Tamizhvendan Sembiyan <tamizhvendan.sembiyan@chargebee.com>
2022-03-15 13:34:41 +0530

    new release

--- dist/index.js
+++ dist/index.js
@@ -8,11 +8,8 @@
  * @param {import('probot').Probot} app
  */
  module.exports = (app) => {
-  app.log("Yay! The app was loaded!");
-  app.on("issues.opened", async (context) => {
-    return context.octokit.issues.createComment(
-      context.issue({ body: "Hello, World!" })
-    );
+  app.on("pull_request.opened", async (context) => {
+    context.log.info(context.payload.pull_request)
   });
 };
 
