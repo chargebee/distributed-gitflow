c87bd9ae6b238db681f35915c81972d40cf70888
Tamizhvendan Sembiyan <tamizhvendan.sembiyan@chargebee.com>
2022-03-15 14:20:20 +0530

    new release

--- dist/index.js
+++ dist/index.js
@@ -9,7 +9,10 @@
  */
  module.exports = (app) => {
   app.on("pull_request.opened", async (context) => {
-    context.log.info(context.payload.pull_request)
+    context.log.info(JSON.stringify(context.payload.pull_request, null, 2))
+  });
+  app.on("pull_request.closed", async (context) => {
+    context.log.info(JSON.stringify(context.payload.pull_request, null, 2))
   });
 };
 
