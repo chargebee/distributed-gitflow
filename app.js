2dea6eca0906f8dd7c2adde76d129acf2177e64e
Tamizhvendan Sembiyan <tamizhvendan.sembiyan@chargebee.com>
2022-03-15 13:31:21 +0530

    log on pull request open

--- app.js
+++ app.js
@@ -2,9 +2,7 @@
  * @param {import('probot').Probot} app
  */
  module.exports = (app) => {
-  app.on("issues.opened", async (context) => {
-    return context.octokit.issues.createComment(
-      context.issue({ body: "Hello, World!" })
-    );
+  app.on("pull_request.opened", async (context) => {
+    context.log.info(context.payload.pull_request)
   });
 };
