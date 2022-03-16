8922eb25454ef4bd1eb840de41ae4c48b3ce7116
Tamizhvendan Sembiyan <tamizhvendan.sembiyan@chargebee.com>
2022-03-16 12:40:48 +0530

    log repo metadata

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ const { onPrOpen, onPrClose } = require("./handlers/pr");
 module.exports = (app) => {
   app.on("pull_request.opened", onPrOpen);
   app.on("pull_request.closed", onPrClose);
+  app.on("issues.opened", async (context) => {
+    context.log.info(context.repo)
+  })
   app.onError(async (error) => {
-    console.error(error)
+    console.error(error, 1)
   })
 };
