cf01c404af57b965840493aa6d8b26188dda19e0
Tamizhvendan Sembiyan <tamizhvendan.sembiyan@chargebee.com>
2022-03-16 14:27:39 +0530

    try with only one branch for PR create

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const github = require("./apps/github")
 
 async function fetchingStagingBranchNames(octokit) {
   let branchNames = await github.fetchProtectedBranchNames(octokit, process.env.REPO_OWNER, process.env.REPO_NAME)
-  return branchNames.filter(branchName => branchName.startsWith("staging/"))
+  let names = branchNames.filter(branchName => branchName.startsWith("staging/"))
+  return [names[0]]
 }
 
 module.exports = (app) => {
@@ -15,7 +16,6 @@ module.exports = (app) => {
   app.on("pull_request.closed", onPrClose);
   app.on("issues.opened", async (context) => {
     let stagingBranchNames = await fetchingStagingBranchNames(context.octokit)
-    stagingBranchNames.forEach(console.log)
     let promises = stagingBranchNames.map(async (branchName) => {
       await github.createPr(context.octokit, process.env.REPO_OWNER, process.env.REPO_NAME, "master", branchName, "Syncing with Upstream Branch")
     })
