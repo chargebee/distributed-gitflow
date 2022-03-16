6117902a57fdfe77d7858cbca67556bfdd2de3c8
Tamizhvendan Sembiyan <tamizhvendan.sembiyan@chargebee.com>
2022-03-16 13:11:03 +0530

    add branch listing log

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ module.exports = (app) => {
   app.on("pull_request.opened", onPrOpen);
   app.on("pull_request.closed", onPrClose);
   app.on("issues.opened", async (context) => {
-    context.log.info(context.repo)
+    let branches = await context.octokit.repos.listBranches({owner: process.env.REPO_OWNER, repo: process.env.REPO_NAME, protected : false})
+    branches.data.forEach(branch => {console.log(branch.name)})
   })
   app.onError(async (error) => {
     console.error(error, 1)
