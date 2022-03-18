97a912f511b493bfa26fccb3123a2bb99d5f0322
Tamizhvendan Sembiyan <tamizhvendan.sembiyan@chargebee.com>
2022-03-18 15:00:45 +0530

    comment local dev codebase

--- app.js
+++ app.js
@@ -7,10 +7,9 @@ const { onPrOpen, onPrClose } = require("./handlers/pr");
 module.exports = (app) => {
   app.on("pull_request.opened", onPrOpen);
   app.on("pull_request.closed", onPrClose);
-  app.on("issues.opened", async context => {
-    let pulls = await context.octokit.pulls.list(context.repo({state: "open", base : "chore/c-123", head: "master"}))
-    console.log(JSON.stringify(pulls, null, 2))
-  })
+  // app.on("issues.opened", async context => {
+    // For Local Dev
+  // })
   app.onError(async (error) => {
     core.setFailed(error)
   })
