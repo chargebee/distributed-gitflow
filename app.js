66e9d1c7210a897e5718078fe05bd204d4ca95b9
Tamizhvendan Sembiyan <tamizhvendan.sembiyan@chargebee.com>
2022-03-18 14:52:01 +0530

    update log

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ module.exports = (app) => {
   app.on("pull_request.opened", onPrOpen);
   app.on("pull_request.closed", onPrClose);
   app.on("issues.opened", async context => {
-    let pulls = await context.octokit.pulls.list(context.repo({state: "open", base : "staging/subscriptions", head: "master"}))
+    let pulls = await context.octokit.pulls.list(context.repo({state: "open", base : "chore/c-123", head: "master"}))
     console.log(JSON.stringify(pulls, null, 2))
   })
   app.onError(async (error) => {
